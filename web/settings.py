c1e065ecd7a7393cb1c95053fc1b236926224f72
mmclar <mmclar@8335caa1-8945-4420-9511-41267666d65f>
2011-02-17 20:18:46 +0000

    change templates dir to be absolute path

--- web/settings.py
+++ web/settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Django settings for beer_web project.
 
 DEBUG = True
@@ -81,7 +83,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'templates'
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
