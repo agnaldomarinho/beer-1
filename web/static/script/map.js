8bbc53e1466190b32b8f35512e3e36e0c7f1e462
mmclar <mmclar@8335caa1-8945-4420-9511-41267666d65f>
2011-03-02 02:58:03 +0000

    fixed some typos and got features working

--- web/static/script/map.js
+++ web/static/script/map.js
@@ -6,20 +6,28 @@ var Map = function(){
     return {
         Initialize: function(){
         	map = new OpenLayers.Map('map');
-			var gmap = new OpenLayers.Layer.Google(
-				"Google Streets",
-				{numZoomLevels: 20}
+			var base = new OpenLayers.Layer.OSM("Google Streets");
+			Map.Bars = new OpenLayers.Layer.Vector(
+				"Bar Markers",
+				{
+					styleMap: new OpenLayers.StyleMap({
+						externalGraphic: "/static/script/ol/img/marker.png",
+						backgroundXOffset: 0,
+						backgroundYOffset: -7,
+						pointRadius: 10
+					}),
+					isBaseLayer: false,
+					rendererOptions: { yOrdering: true }
+				}
 			);
-			Map.Bars = new OpenLayers.Layer.Markers("Bar Markers");
-			map.addLayers([gmap, Map.Bars]);
-			map.setCenter(Map.Project(-75.16, 39.963), 14);
-			var controls = [
-				new OpenLayers.Control.DragMarker(Map.Bars)
-			];
-			for (control in controls){
-				map.addControls(control);
-			}
-            $.ajax({
+			map.addLayers([base, Map.Bars]);
+			map.setCenter(Map.ProjectLonLat(-75.16, 39.963), 16);
+			
+			var dragFeature = new OpenLayers.Control.DragFeature(Map.Bars);
+			map.addControl(dragFeature);
+			dragFeature.activate();
+            
+			$.ajax({
                 url: "/bars/(1.1,1.1)/",
                 success: Map.DisplayMarkers,
                 dataType: "json"
@@ -28,18 +36,28 @@ var Map = function(){
         },
 
         DisplayMarkers: function(items){
-            for (var bar in items.bars){
-                var marker = new OpenLayers.Marker(Map.Project(bar.location[0], bar.location[1]));
-				Map.Bars.addMarker(marker);
+			Map.Bars.removeFeatures(Map.Bars.features);
+            for each (var bar in items.bars){
+                var feature = new OpenLayers.Feature.Vector(
+					Map.ProjectPoint(bar.location[0], bar.location[1])
+				);
+				Map.Bars.addFeatures(feature);
             }
         },
 
-		Project: function(lon, lat) {
-			return new OpenLayers.LonLat(lon, lat).transform(
+		ProjectLonLat: function(lon, lat) {
+			return Map.Project(lon, lat, OpenLayers.LonLat);
+		},
+
+		ProjectPoint: function(x, y) {
+			return Map.Project(x, y, OpenLayers.Geometry.Point);
+		},
+
+		Project: function(x, y, t) {
+			return new t(x, y).transform(
 				new OpenLayers.Projection("EPSG:4326"),
 				map.getProjectionObject()
 			);
-
 		}
     };
 }();
